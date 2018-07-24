88688b9f3c34a299493f86a9dd4b12bf02203398
Nachiket Kulkarni <nachiket_kulkarni@apple.com>
2018-07-24 14:02:18 -0700

    Move to the shared code

--- keyboards/paladinsplit/right/config.h
+++ keyboards/paladinsplit/right/config.h
@@ -20,44 +20,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../config.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xCEEB
-#define PRODUCT_ID      0x1157
-#define DEVICE_VER      0x0100
-#define MANUFACTURER    Nachie
-#define PRODUCT         The Paladin Split Right
-#define DESCRIPTION     Split 5x14 ortholinear keyboard
-
-/* key matrix size */
-// Rows are doubled-up
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 8
-
 // wiring of each half
 #define MATRIX_ROW_PINS { D3, B6, B5, B4, D7 }
 #define MATRIX_COL_PINS { B7, D0, F6, F5, F4, F1, F0, D1 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* number of backlight levels */
-// #define BACKLIGHT_LEVELS 3
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN F7
